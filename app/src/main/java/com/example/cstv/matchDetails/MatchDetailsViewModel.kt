41017d2b1ad5fb1018f35f0546e2c81bf8ff4fad
LuizGusttavo <luiz.gusttavo@lavid.ufpb.br>
2022-04-08 03:30:36 -0300

    Repository modified

--- app/src/main/java/com/example/cstv/matchDetails/MatchDetailsViewModel.kt
+++ app/src/main/java/com/example/cstv/matchDetails/MatchDetailsViewModel.kt
@@ -3,12 +3,14 @@ package com.example.cstv.matchDetails
 import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.cstv.entities.TeamsItem
 import com.example.cstv.service.RetrofitClient
 import com.example.cstv.service.listeners.TeamsListeners
 import com.example.cstv.service.repository.RepositoryTeams
+import kotlinx.coroutines.launch
 
-class MatchDetailsViewModel: ViewModel() {
+class MatchDetailsViewModel : ViewModel() {
 
     private val mRepositoryTeams = RepositoryTeams()
 
@@ -18,30 +20,18 @@ class MatchDetailsViewModel: ViewModel() {
     private var mTeam2 = MutableLiveData<TeamsItem?>()
     var team2 = mTeam2
 
+    private lateinit var teams: List<TeamsItem>
     private lateinit var errorMessage: String
+
 
     fun listTeams(teamNames: String) {
 
-        mRepositoryTeams.listTeams(
-            RetrofitClient.TOKEN,
-            teamNames,
-            object : TeamsListeners {
-
-                override fun onSucces(entity: List<TeamsItem>) {
-                    team1.value = entity[0]
-                    if(entity.size == 2) {
-                        team2.value = entity[1]
-                    }else{
-                        team2.value = null
-                    }
-                }
-
-                override fun onFailure(message: String) {
-                    errorMessage = message
-                    Log.e("MatchDetails", "onFailure: $errorMessage", )
-                }
-
-            })
+        viewModelScope.launch {
+            teams = mRepositoryTeams.listTeams(
+                RetrofitClient.TOKEN,
+                teamNames
+            )!!
+        }
 
 
     }
--- app/src/main/java/com/example/cstv/service/requests/RequestTeams.kt
+++ app/src/main/java/com/example/cstv/service/requests/RequestTeams.kt
@@ -1,8 +1,6 @@
 package com.example.cstv.service.requests
 
-import com.example.cstv.entities.MatchItem
 import com.example.cstv.entities.TeamsItem
-import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.Query
@@ -13,8 +11,8 @@ interface RequestTeams {
     //usando comma-separate como: "MAD Lions,BlackBelt"
 
     @GET("csgo/teams")
-    fun listTeams(
+    suspend fun listTeams(
         @Header("Authorization") token: String,
         @Query("filter[name]") teamNames: String,
-    ): Call<List<TeamsItem>>
+    ): List<TeamsItem>
 }
